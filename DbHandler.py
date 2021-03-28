6edca06743707ab0e1d93b89265957813376c331
romeoSTR <twilizerov@gmail.com>
2021-03-28 21:29:12 +0300

    add update rating func

--- DbHandler.py
+++ DbHandler.py
@@ -6,5 +6,13 @@ def init_db(instance):
 	else 
 		print('db getted')
 
-def get_all_questions_from_db(session)
+def get_all_questions_from_db(session):
 	print('questions getted')
+
+
+def delete_user(session):
+	print('user deleted')
+
+
+def update_user_rating(session):
+	print('rating updated')
